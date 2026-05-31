70bd4e4cf2611493b39cedc7ad3baf2843b7b2ba
mohamedeld <mohamed.azoz20010@gmail.com>
2026-05-31 12:07:46 +0300

    add meeting data

--- src/modules/meetings/ui/views/meetings-view.tsx
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -1,10 +1,29 @@
 "use client";
 
+import { DataTable } from "@/modules/agents/ui/components/data-table";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { columns } from "../components/meeting-columns";
+import { EmptyState } from "@/components/empty-state";
 
 export const MeetingsView = () => {
   const trpc = useTRPC();
+  const router = useRouter();
   const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+  return (
+    <div className="overflow-x-auto flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+      <DataTable
+        data={data?.items}
+        columns={columns}
+        onRowClick={(row) => router.push(`/meetings/${row?.id}`)}
+      />
+      {data?.items?.length === 0 && (
+        <EmptyState
+          title="Create your first meeting"
+          description="Schedule a meeting to connect with others. Each meeting lets you collaborate, share ideas, and interact with participants in real time."
+        />
+      )}
+    </div>
+  );
 };
